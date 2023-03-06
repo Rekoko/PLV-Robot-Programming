0181632e003ea3b64303b1f6e3ab5bd27259c04f
Daniel Biberger <daniel.biberger@yahoo.de>
2023-03-06 12:20:30 +0100

    function layout added

--- solution5.py
+++ solution5.py
@@ -1,17 +1,23 @@
 import rclpy  # ROS client library
-import os
-import subprocess
 from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
-from enum import Enum, auto
+
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+
+
+from transforms3d import euler
+from enum import Enum, auto
+
 
 class State(Enum):
     TO_THE_FIRST_WALL = auto()
-    ROTATING = auto()
+    ROTATING_LEFT = auto()
     TO_THE_SECOND_WALL = auto()
     STOP = auto()
+    CHECK_ROTATION = auto()
+
 
 class Tb3(Node):
     def __init__(self):
@@ -27,12 +33,18 @@ class Tb3(Node):
                 'scan',
                 self.scan_callback,  # function to run upon message arrival
                 qos_profile_sensor_data)  # allows packet loss
+        
+        self.scan_sub = self.create_subscription(
+                Odometry,
+                'odom',
+                self.odom_callback,  # function to run upon message arrival
+                qos_profile_sensor_data)  # allows packet loss
+        
 
-        self.st = State.TO_THE_FIRST_WALL
         self.ang_vel_percent = 0
         self.lin_vel_percent = 0
-    
-
+        self.state = State.ROTATING_LEFT
+        self.diff = 0
 
     def vel(self, lin_vel_percent, ang_vel_percent=0):
         """ publishes linear and angular velocities in percent
@@ -52,32 +64,55 @@ class Tb3(Node):
     def scan_callback(self, msg):
         """ is run whenever a LaserScan msg is received
         """
-        print()
-        print('⬆️ :', msg.ranges[0])
-        print('⬇️ :', msg.ranges[180])
-        print('⬅️ :', msg.ranges[90])
-        print('➡️ :', msg.ranges[-90])
-        
+        # print()
+        # print('Distances:')
+        # print('⬆️ :', msg.ranges[0])
+        # print('⬇️ :', msg.ranges[180])
+        # print('⬅️ :', msg.ranges[90])
+        #print('➡️ :', msg.ranges[-90])
+        if self.state == State.TO_THE_FIRST_WALL:
+            if msg.ranges[0] > 0.25:
+                self.vel(20)
+            else:
+                self.state = State.ROTATING_LEFT
+        if self.state == State.ROTATING_LEFT:
+            if msg.ranges[-90] > 0.179:
+                self.vel(0, ang_vel_percent=10)
+            else:
+                self.state = State.TO_THE_SECOND_WALL
+        if self.state == State.CHECK_ROTATION:
+            pass    
 
-
-        if self.st == State.TO_THE_FIRST_WALL:
-            self.vel(20)
-            if msg.ranges[0] <= 0.25:
-                self.st = State.ROTATING
-        elif self.st == State.ROTATING:
-            self.vel(0, 20)
-            if msg.ranges[-90] <= 0.17:
-                self.st = State.TO_THE_SECOND_WALL
-        elif self.st == State.TO_THE_SECOND_WALL:
-            self.vel(20)
-            if msg.ranges[0] < 0.2:
-                self.vel(0)
-                self.st = State.STOP
-        elif self.st == State.STOP:
+        if self.state == State.TO_THE_SECOND_WALL:
+            if msg.ranges[0] > 0.22:
+                self.vel(20)
+            else:
+                self.state = State.STOP
+        if self.state == State.STOP:
             self.vel(0)
+        self.diff = msg 
 
+    def odom_callback(self, msg):
+        pos = msg.pose.pose.position
+        ori = msg.pose.pose.orientation
+        # print(msg.pose.pose.position)
+        # print(msg.pose.pose.orientation)
+        # print(ori)
+        eul_z,_,_ = euler.quat2euler((ori.w, ori.x, ori.y, ori.z), "szyx")
+        print(eul_z)
+        pass
 
+    def driveToCords(self, x, y):
+        return None
+    
+    def speedRegulation(self):
+        return None
+    
+    def checkGoald(self):
+        return None
 
+    def calculatePath(self):
+        return None
 
 def main(args=None):
     rclpy.init(args=args)
